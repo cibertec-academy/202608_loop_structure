b8b857cb08c8f7d1fe3e38450d405f08aeaaf176
Gian Carlo Quiroz Guzman <gcquirozguzman@gmail.com>
2026-08-14 22:11:33 -0500

    Fuentes iniciales del proyecto

--- src/www/cibertec/Sistema_Gian_Quiroz.java
+++ src/www/cibertec/Sistema_Gian_Quiroz.java
@@ -3,7 +3,7 @@ package www.cibertec;
 public class Sistema_Gian_Quiroz {
 
     public static void main(String[] args) {
-        System.out.println("Hola mundo 2");
+        System.out.println("Hola mundo 22");
     }
 
 }
