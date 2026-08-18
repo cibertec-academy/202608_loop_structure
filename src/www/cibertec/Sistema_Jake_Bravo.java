58b3f93e2e152b5a16ed6d108e6357a6b0eb1f16
Gian Carlo Quiroz Guzman <gcquirozguzman@gmail.com>
2026-08-17 22:09:05 -0500

    Archivo nuevo de prueba

--- src/www/cibertec/Sistema_Jake_Bravo.java
+++
@@ -1,9 +0,0 @@
-package www.cibertec;
-
-public class Sistema_Jake_Bravo {
-
-    public static void main(String[] args) {
-        System.out.println("Hola mundo mi nombre es Jake Bravo");
-    }
-
-}
